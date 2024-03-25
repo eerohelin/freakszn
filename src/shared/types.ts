2ab99f029a8d4f8e9a9c5bd0f7cd80bbdf7798ff
Riku Rainio <76222709+rikurainio@users.noreply.github.com>
2024-03-25 12:06:17 +0200

    remove unused type

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,9 +1,3 @@
 // custom type definitions can live here
-
-// example global state for an application with customization options
-export type GlobalState = {
-  colorMode: "dark" | "light";
-};
-
 export type LobbyPosition = "one" | "two" | "spectator"
 export type DraftMode = 1 | 6
