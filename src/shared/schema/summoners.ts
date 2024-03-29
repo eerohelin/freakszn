9bb265dc1c07325f376d62c3913a2e06942bbec4
Riku Rainio <76222709+rikurainio@users.noreply.github.com>
2024-03-29 22:17:16 +0200

    add summoner schema

---
+++ src/shared/schema/summoners.ts
@@ -0,0 +1,14 @@
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const summoners = sqliteTable("summoners", {
+  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  accountId: integer("accountId").notNull(),
+  displayName: text("displayName").notNull(),
+  gameName: text("gameName").notNull(),
+  internalName: text("internalName").notNull(),
+  profileIconId: integer("profileIconId").notNull(),
+  puuid: text("puuid").notNull(),
+  summonerId: integer("summonerId").notNull(),
+  summonerLevel: integer("summonerLevel").notNull(),
+  tagLine: text("tagLine").notNull(),
+});
