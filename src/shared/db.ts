6fad79cde3874da5b5c10ec7cfed4a4647b2e09d
Riku Rainio <76222709+rikurainio@users.noreply.github.com>
2024-03-29 22:17:42 +0200

    add some summoner actions

--- src/shared/db.ts
+++ src/shared/db.ts
@@ -5,3 +5,49 @@ import * as schema from "./schema/index";
 
 const sqlite = new Database(`${app.getPath("userData")}/db.sqlite`);
 export const db = drizzle(sqlite, { schema });
+
+/** DB ACTIONS */
+export async function updateSummoner(summoner: Summoner): Promise<void> {
+  try {
+    await db.insert(schema.summoners).values({
+      id: 1,
+      ...summoner
+    }).onConflictDoUpdate({
+      target: schema.summoners.id,
+      set: { 
+        accountId: summoner.accountId,
+        displayName: summoner.displayName,
+        gameName: summoner.gameName,
+        internalName: summoner.gameName,
+        profileIconId: summoner.profileIconId,
+        summonerId: summoner.summonerId,
+        summonerLevel: summoner.summonerLevel,
+        puuid: summoner.puuid,
+        tagLine: summoner.tagLine
+      }
+    })
+  } catch (error) { console.log('error:', error )}
+
+}
+
+export async function getSummoner(): Promise<Summoner[]> {
+  const summoners = await db.select().from(schema.summoners)
+  return summoners
+}
+
+export async function deleteSummoners(): Promise<void> {
+  await db.delete(schema.summoners)
+}
+
+/** DB TYPES */
+export interface Summoner {
+  accountId: number
+  displayName: string
+  gameName: string
+  internalName: string
+  profileIconId: number
+  puuid: string
+  summonerId: number
+  summonerLevel: number
+  tagLine: string
+}
