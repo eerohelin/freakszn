996b336193cebfe02aac2c5965f8d0115bd29c39
Riku Rainio <76222709+rikurainio@users.noreply.github.com>
2024-03-30 18:32:47 +0200

    add profile image url util

--- src/web/lib/utils.ts
+++ src/web/lib/utils.ts
@@ -5,3 +5,9 @@ import { clsx } from "clsx";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getProfileSplashUrl(backgroundSkinId?: number): string {
+  if(!backgroundSkinId){ return "" }
+  const championId = backgroundSkinId.toString().slice(0, -3)
+  return `https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/${championId}/${backgroundSkinId}.jpg`
+}
