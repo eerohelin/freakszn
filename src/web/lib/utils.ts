1c252af782c467e662335d5c6e36a0a6091748d7
Riku Rainio <76222709+rikurainio@users.noreply.github.com>
2024-03-31 01:52:10 +0200

    add banner util

--- src/web/lib/utils.ts
+++ src/web/lib/utils.ts
@@ -11,3 +11,15 @@ export function getProfileSplashUrl(backgroundSkinId?: number): string {
   const championId = backgroundSkinId.toString().slice(0, -3)
   return `https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/${championId}/${backgroundSkinId}.jpg`
 }
+
+export function getProfileClashBannerUrl(bannerTheme?: string, level?: number): string {
+  if(!bannerTheme || !level){ return ""}
+  /*
+  const response = await fetch("https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/summoner-banners.json")
+  const clashBannerData = await response.json()
+  */
+  const bt = bannerTheme.toLowerCase()
+  const clashBannerUrl = `https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/loadouts/summonerbanners/flags/${bt}/leagueclient/flag_${bt}_${level}_inventory.png`
+  console.log('banner:', clashBannerUrl)
+  return clashBannerUrl
+}
