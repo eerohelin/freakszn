1b9f4c7a4cccfb061a249d5f4f1bbe43cce3c6f5
Riku Rainio <76222709+rikurainio@users.noreply.github.com>
2024-03-30 18:32:19 +0200

    adjust styling

--- src/web/components/buttons.tsx
+++ src/web/components/buttons.tsx
@@ -9,15 +9,16 @@ interface ButtonProps
 }
 
 /** League button style by default */
-export function Button({ children, className, type }: ButtonProps) {
+export function Button({ children, className, type, onClick }: ButtonProps) {
   return (
     <button
+      onClick={onClick}
       type={type || "button"}
       className={cn(
         className,
         `flex items-center content-center w-full justify-center bg-gradient-to-b from-league-border 
         to-league-borderdarker p-[.125rem] uppercase tracking-widest  
-         first-letter:w-full hover:brightness-150 transition-all duration-150`,
+         first-letter:w-full hover:brightness-[175%] transition-all duration-150`,
       )}
     >
       <span className={cn("font-beaufort-bold",
