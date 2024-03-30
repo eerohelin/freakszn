5692062eaa0ee20f916668a9b68239a0c5e5b433
Riku Rainio <76222709+rikurainio@users.noreply.github.com>
2024-03-30 02:11:27 +0200

    use socket provider

--- src/web/main.tsx
+++ src/web/main.tsx
@@ -3,9 +3,8 @@ import React from "react";
 import t, { queryClient, trpcClient } from "@shared/config";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ThemeProvider } from "./components/providers";
+import { SocketProvider, ThemeProvider } from "./components/providers";
 import { createRoot } from "react-dom/client";
-import { type Socket, io } from "socket.io-client";
 import { routeTree } from "./routeTree.gen";
 
 /** Provider routing */
@@ -16,20 +15,18 @@ declare module "@tanstack/react-router" {
   }
 }
 
-/** Provide socket */
-const SocketContext = React.createContext<Socket | undefined>(undefined);
 
 /** Declare providers here. Can be moved to separate Providers.tsx later */
 createRoot(document.getElementById("app") as Element).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <SocketContext.Provider value={io("ws//localhost:3000")}>
-        <t.Provider client={trpcClient} queryClient={queryClient}>
-          <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router} />
-          </QueryClientProvider>
-        </t.Provider>
-      </SocketContext.Provider>
-    </ThemeProvider>
+    <SocketProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <t.Provider client={trpcClient} queryClient={queryClient}>
+            <QueryClientProvider client={queryClient}>
+              <RouterProvider router={router} />
+            </QueryClientProvider>
+          </t.Provider>
+      </ThemeProvider>
+    </SocketProvider>
   </React.StrictMode>,
 );
