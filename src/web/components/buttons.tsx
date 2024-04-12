9b2553de2cbb6f393bac4c30655fa17e745c1f6c
Riku Rainio <76222709+rikurainio@users.noreply.github.com>
2024-04-12 19:15:31 +0300

    styling

--- src/web/components/buttons.tsx
+++ src/web/components/buttons.tsx
@@ -16,7 +16,7 @@ export function Button({ children, className, type, onClick }: ButtonProps) {
       type={type || "button"}
       className={cn(
         `flex items-center content-center w-full justify-center bg-gradient-to-b from-league-border 
-        to-league-borderdarker p-[.125rem] uppercase tracking-widest  
+        to-league-borderdarker p-[.125rem] uppercase tracking-widest text-league-text 
         first-letter:w-full hover:brightness-[175%] transition-all duration-150`,
         className,
       )}
@@ -25,7 +25,7 @@ export function Button({ children, className, type, onClick }: ButtonProps) {
         className={cn(
           "font-beaufort-bold",
           `flex w-full items-center justify-center content-center border-b border-x border-black
-            px-4 py-2 bg-league-button text-league-text text-center`,
+            px-4 py-2 bg-league-button text-center`,
         )}
       >
         {children}
