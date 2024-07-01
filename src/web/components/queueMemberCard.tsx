2b51310a2d535aa526d4faf8fca7e29ba4989237
Riku Rainio <76222709+rikurainio@users.noreply.github.com>
2024-07-01 18:18:26 +0300

    styling

--- src/web/components/queueMemberCard.tsx
+++ src/web/components/queueMemberCard.tsx
@@ -8,7 +8,7 @@ const QueueMemberCard = ({ className, playerName }: QueueMemberCardProps) => {
   return (
     <div 
         className={cn(
-            "p-[0.06rem] bg-gradient-to-br from-gray-500 to-text rounded-[0.375rem]", 
+            "p-[0.06rem] bg-gradient-to-br from-gray-500 to-text rounded-[0.375rem] shadow hover:bg-indigo-500 transition-all duration-1000", 
             className
         )}>
         <div className='w-full h-full bg-card px-3 rounded-[0.23rem]'>
