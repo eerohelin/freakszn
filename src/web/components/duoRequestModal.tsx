154763b72189de7bc76f7637f61f813d53619bb1
Riku Rainio <76222709+rikurainio@users.noreply.github.com>
2024-07-01 21:06:50 +0300

    move to separate

---
+++ src/web/components/duoRequestModal.tsx
@@ -0,0 +1,148 @@
+import React, { useContext } from "react";
+import { SocketProviderContext } from "./providers";
+import { useSummonerIcon } from "../hooks/useSummonerIcon";
+import { ButtonFszn } from "./buttons";
+import { ROLES } from "../lib/constants";
+import { cn } from "../lib/utils";
+
+interface DuoRequestModalProps {
+    show: boolean
+    setShow: React.Dispatch<React.SetStateAction<boolean>>
+    player: any
+  }
+
+export const DuoRequestModal = ({ player, show, setShow }: DuoRequestModalProps) => {
+    const { socket } = useContext(SocketProviderContext);
+    const { i } = useSummonerIcon(Number(player?.iconId));
+    const { i: placeholder } = useSummonerIcon(29);
+    const [myRole, setMyRole] = React.useState<string>()
+    const [hisRole, setHisRole] = React.useState<string>()
+    const [showMyRolePicker, setShowMyRolePicker] = React.useState(false);
+    const [showHisRolePicker, setShowHisRolePicker] = React.useState(false);
+  
+    function clearFields() {
+      setMyRole(undefined)
+      setHisRole(undefined)
+      setShowMyRolePicker(false)
+      setShowHisRolePicker(false)
+    }
+  
+    return (
+        <> { show &&
+        <div className={cn(
+            "fixed inset-0 w-full h-full flex flex-col items-center content-center",
+            "justify-center bg-black backdrop-blur-sm bg-opacity-30 z-[1000]"
+        )}>
+            <div className="bg-card p-6">
+
+            {/** PLAYER INFO */}
+            <div className="flex gap-2 items-center pb-4">
+                <div className="flex flex-col items-center content-center justify-center w-full">
+                <div className="flex items-center">
+                    <div
+                    className="bg-gradient-to-b from-league-border 
+
+                    to-league-borderdarker p-[0.05rem]"
+                    >
+                    <img
+                        className="w-24"
+                        src={i.length > 1000 ? i : placeholder}
+                        alt="avatar"
+                    />
+                    </div>
+                </div>
+                <h1 className="text-2xl mt-1">Ask {player.name} to duo</h1>
+                </div>
+            </div>
+
+            <div className="pb-4 flex flex-col gap-2">
+                <ButtonFszn
+                className={cn((showMyRolePicker || showHisRolePicker) && "hidden", "bg-white/20")}
+                onClick={() => {
+                    setShowHisRolePicker(false);
+                    setShowMyRolePicker(true);
+                }}>
+                Your role ({myRole})
+                </ButtonFszn>
+                { showMyRolePicker &&
+                <>
+                    <span>Your role</span>
+                    <div className="flex items-center gap-2">
+                    {ROLES.map((r: string) => 
+                    <ButtonFszn
+                        key={r}
+                        onClick={() => {
+                        setMyRole(r.toLowerCase())
+                        setShowMyRolePicker(false);
+                        }}
+                        className={cn(
+                        hisRole?.toLowerCase() === r.toLowerCase() && "pointer-events-none grayscale", "bg-white/20"
+                    )}>
+                        {r}
+                    </ButtonFszn>)}
+                    </div>
+                </>
+                }
+                
+                <ButtonFszn
+                className={cn((showMyRolePicker || showHisRolePicker) && "hidden", "bg-white/20")}
+                onClick={() => {
+                    setShowMyRolePicker(false);
+                    setShowHisRolePicker(true);
+                }}>
+                Your duos role ({hisRole})
+                </ButtonFszn>
+                { showHisRolePicker &&
+                <>
+                    <span>Your duos role</span>
+                    <div className="flex items-center gap-2">
+                    {ROLES.map((r: string) => 
+                    <ButtonFszn
+                        key={r}
+                        onClick={() => {
+                        setHisRole(r.toLowerCase())
+                        setShowHisRolePicker(false);
+                        }}
+                        className={cn(
+                        myRole?.toLowerCase() === r.toLowerCase() && "pointer-events-none grayscale", "bg-white/20"
+                    )}>
+                        {r}
+                    </ButtonFszn>)}
+                    </div>
+                </>
+                }
+            </div>
+
+            {/** BUTTONS ROW */}
+            <div className="flex items-center gap-2 mt-6">
+                <ButtonFszn
+                disabled={myRole === hisRole || myRole === undefined || hisRole === undefined}
+                className={cn(
+                    "bg-white/20",
+                    (myRole === hisRole || myRole === undefined || hisRole === undefined) && "pointer-events-none bg-black/30 text-white/20",
+                )}
+                onClick={() => {
+                    socket?.emit("duo-queue", {
+                    duoName: player.name,
+                    duoTagline: player.tagline,
+                    myRole: myRole,
+                    duoRole: hisRole,
+                    })
+                    clearFields()
+                    setShow(false)
+                }}>
+                Ask to duo
+                </ButtonFszn>
+                <ButtonFszn className="bg-white/20" onClick={() => {
+                clearFields()
+                setShow(false)
+                }}>
+                Close
+                </ButtonFszn>
+            </div>
+            </div>
+        </div>
+        }
+        </>
+    )
+}
--- src/web/components/users.tsx
+++ src/web/components/users.tsx
@@ -1,9 +1,6 @@
-import React, { useContext } from "react";
+import React from "react";
 import { SocketProviderContext } from "./providers";
-import { useSummonerIcon } from "../hooks/useSummonerIcon";
-import { ROLES } from "../lib/constants";
-import { Button, ButtonFszn } from "./buttons";
-import { cn } from "../lib/utils";
+import { DuoRequestModal } from "./duoRequestModal";
 import { UserCard } from "./userCard";
 
 interface UsersProps {
@@ -35,7 +32,7 @@ export function Users({ users }: UsersProps) {
               />
             ))}
           </div>
-          <PlayerModal
+          <DuoRequestModal
             setShow={setShowModal}
             player={modalUser} 
             show={showModal}
@@ -44,146 +41,4 @@ export function Users({ users }: UsersProps) {
       </div>
     </>
   );
-}
-
-interface PlayerModalProps {
-  show: boolean
-  setShow: React.Dispatch<React.SetStateAction<boolean>>
-  player: any
-}
-
-const PlayerModal = ({ player, show, setShow }: PlayerModalProps) => {
-  const { socket } = useContext(SocketProviderContext);
-  const { i } = useSummonerIcon(Number(player?.iconId));
-  const { i: placeholder } = useSummonerIcon(29);
-  const [myRole, setMyRole] = React.useState<string>()
-  const [hisRole, setHisRole] = React.useState<string>()
-  const [showMyRolePicker, setShowMyRolePicker] = React.useState(false);
-  const [showHisRolePicker, setShowHisRolePicker] = React.useState(false);
-
-  function clearFields() {
-    setMyRole(undefined)
-    setHisRole(undefined)
-    setShowMyRolePicker(false)
-    setShowHisRolePicker(false)
-  }
-
-  return (
-    <> { show &&
-      <div className={cn(
-        "fixed inset-0 w-full h-full flex flex-col items-center content-center",
-        "justify-center bg-black backdrop-blur-sm bg-opacity-30 z-[1000]"
-      )}>
-        <div className="bg-card p-6">
-
-          {/** PLAYER INFO */}
-          <div className="flex gap-2 items-center pb-4">
-            <div className="flex flex-col items-center content-center justify-center w-full">
-              <div className="flex items-center">
-                <div
-                  className="bg-gradient-to-b from-league-border 
-
-                to-league-borderdarker p-[0.05rem]"
-                >
-                  <img
-                    className="w-24"
-                    src={i.length > 1000 ? i : placeholder}
-                    alt="avatar"
-                  />
-                </div>
-              </div>
-              <h1 className="text-2xl mt-1">Ask {player.name} to duo</h1>
-            </div>
-          </div>
-
-          <div className="pb-4 flex flex-col gap-2">
-            <ButtonFszn
-              className={cn((showMyRolePicker || showHisRolePicker) && "hidden", "bg-white/20")}
-              onClick={() => {
-                setShowHisRolePicker(false);
-                setShowMyRolePicker(true);
-            }}>
-              Your role ({myRole})
-            </ButtonFszn>
-            { showMyRolePicker &&
-              <>
-                <span>Your role</span>
-                <div className="flex items-center gap-2">
-                  {ROLES.map((r: string) => 
-                  <ButtonFszn
-                    key={r}
-                    onClick={() => {
-                      setMyRole(r.toLowerCase())
-                      setShowMyRolePicker(false);
-                    }}
-                    className={cn(
-                    hisRole?.toLowerCase() === r.toLowerCase() && "pointer-events-none grayscale", "bg-white/20"
-                  )}>
-                    {r}
-                  </ButtonFszn>)}
-                </div>
-              </>
-            }
-            
-            <ButtonFszn
-              className={cn((showMyRolePicker || showHisRolePicker) && "hidden", "bg-white/20")}
-              onClick={() => {
-                setShowMyRolePicker(false);
-                setShowHisRolePicker(true);
-            }}>
-              Your duos role ({hisRole})
-            </ButtonFszn>
-            { showHisRolePicker &&
-              <>
-                <span>Your duos role</span>
-                <div className="flex items-center gap-2">
-                  {ROLES.map((r: string) => 
-                  <ButtonFszn
-                    key={r}
-                    onClick={() => {
-                      setHisRole(r.toLowerCase())
-                      setShowHisRolePicker(false);
-                    }}
-                    className={cn(
-                    myRole?.toLowerCase() === r.toLowerCase() && "pointer-events-none grayscale", "bg-white/20"
-                  )}>
-                    {r}
-                  </ButtonFszn>)}
-                </div>
-              </>
-            }
-          </div>
-
-          {/** BUTTONS ROW */}
-          <div className="flex items-center gap-2 mt-6">
-            <ButtonFszn
-              disabled={myRole === hisRole || myRole === undefined || hisRole === undefined}
-              className={cn(
-                "bg-white/20",
-                (myRole === hisRole || myRole === undefined || hisRole === undefined) && "pointer-events-none bg-black/30 text-white/20",
-              )}
-              onClick={() => {
-                socket?.emit("duo-queue", {
-                  duoName: player.name,
-                  duoTagline: player.tagline,
-                  myRole: myRole,
-                  duoRole: hisRole,
-                })
-                clearFields()
-                setShow(false)
-            }}>
-              Ask to duo
-            </ButtonFszn>
-            <ButtonFszn className="bg-white/20" onClick={() => {
-              clearFields()
-              setShow(false)
-            }}>
-              Close
-            </ButtonFszn>
-          </div>
-        </div>
-      </div>
-    }
-    </>
-  )
 }
